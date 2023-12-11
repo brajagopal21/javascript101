ee4e695185a529242c37c402d96c2c4f7c1b62a8
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-11 07:16:17 +0000

    conversion

---
+++ 01_basic/conversion_operation.js
@@ -0,0 +1,30 @@
+let score = true;
+console.log(typeof score);
+
+
+
+let valurInNumber = Number(score);
+console.log(valurInNumber);
+
+
+//Nan is special type
+
+
+//"33" => 33
+// "33abc"  ==> Nan
+//true => 1; false =>0
+
+let isLoggedIn="string"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
