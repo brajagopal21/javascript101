79ab83d8ac2ec7226548d1c821d6c22a51d54703
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-11 07:55:49 +0000

    some change

--- 01_basic/conversion_operation.js
+++ 01_basic/conversion_operation.js
@@ -28,3 +28,37 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//Operation
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2=" world"
+str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+
+
+console.log("2"+1);
+
+
+console.log(1+2+"3");
+console.log("2"+1+2);
+
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter);
+
+let x=4
+const y=x++
+console.log(x,y);
+
