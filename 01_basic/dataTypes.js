a0f9721f53726b6682b01286b7b1a80bb37b3dac
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-11 06:55:00 +0000

    datatypes and variables

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";//trear all js code as newer standard
+
+// alert("Hello World");//as its using node js runtime not a v8
+
+
+let name ="Braja"
+let number=12133;
+let isLoggedIn=true;
+let a=null;
+
+//number  -->2 power 53  when it exceds we can use bigint although we don't use in regular life
+//String  -->we can use " " or ' '
+//Boolean  ->true or false
+
+//null  -->standalone value 
+
+//undefined
+
+//symbol -->unique
+
+//object
+
+//null is a type of object 
+
+console.log(typeof undefined);
---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountId=121313;
+let accountEmail="b@gmail.com";
+var accoutPassword="12345";
+let accountCity="Chennai";
+let accountHolder;
+
+accountEmail="d@gmail.com";
+accountCity="Erode"
+accountPassword="62627";
+
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountCity,accoutPassword,accountId,accountHolder])
+
