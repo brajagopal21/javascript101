2c444ce27cc680392e8f5aa168ba2c14c6f15d32
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-11 08:31:14 +0000

    datatype summery

---
+++ BASIC/dataTypes-summery.js
@@ -0,0 +1,24 @@
+// console.log(null>0);
+// console.log(typeof null);
+
+/*
+Symbol is basically used as special case
+*/
+
+const id = Symbol("123456");
+const anotherId = Symbol("123456");
+console.log(id == anotherId);
+
+const heros = ["a", "b", "c"];
+let obj = {
+  name: "djfkj",
+  age: 23,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+  return muFunction;
+};
+console.log(typeof obj)
+console.log(typeof myFunction)
+console.log(typeof heros)
