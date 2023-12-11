5ef5452622a4a3a3ea1a3e9c0a0a783acda563fb
Brajagopal Mukherjee <106516823+brajagopal21@users.noreply.github.com>
2023-12-11 11:11:23 +0000

    minor change

---
+++ BASIC/nums_and_math.js
@@ -0,0 +1,10 @@
+const score = 400;
+console.log(score);
+const balance = new Number(100000000000);
+console.log(balance);
+
+// console.log(balance.toFixed(2));
+console.log(balance.toLocaleString());
+
+
+
